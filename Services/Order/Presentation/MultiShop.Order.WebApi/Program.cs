fb9007789827775f3f70185c85eff208bf863034
kosebaris1 <kosebaris464@gmail.com>
2025-10-11 20:10:00 +0300

    feat: CQRS Registration

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,4 +1,9 @@
 
+using MultiShop.Order.Application.Features.CQRS.Handlers.AdressHandlers.Read;
+using MultiShop.Order.Application.Features.CQRS.Handlers.AdressHandlers.Write;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers.Read;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers.Write;
+
 namespace MultiShop.Order.WebApi
 {
     public class Program
@@ -6,6 +11,18 @@ namespace MultiShop.Order.WebApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<GetAdressQueryHandler>();
+            builder.Services.AddScoped<GetAdressByIdQueryHandler>();
+            builder.Services.AddScoped<CreateAdressCommandHandler>();
+            builder.Services.AddScoped<UpdateAdressCommandHandler>();
+            builder.Services.AddScoped<RemoveAdressCommandHandler>();
+
+            builder.Services.AddScoped<GetAllOrderDetailQueryHandler>();
+            builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+            builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+            builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+            builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
 
             // Add services to the container.
 
