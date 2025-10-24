7c3a20bc9c2c6e77d6e68e130b4d8bf577e53fea
kosebaris1 <kosebaris464@gmail.com>
2025-10-24 18:04:40 +0300

    feat: add generic repository and interface implementations

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void delete(int id);
+        void update(T entity);
+
+        T GetById(int id);
+
+        List<T> GetAll();
+    }
+}
